6c30dc37e38f95066b3945e43ed5ce64f1098b78
cloudy9101 <cloudy9101@gmail.com>
2018-12-25 23:49:22 +1300

    P4:(3) Median of Two Sorted Arrays
    Improve algorithm using binary search to reach time complexity of O(logN)

--- p4-MedianOfTwoSortedArrays.go
+++ p4-MedianOfTwoSortedArrays.go
@@ -1,53 +1,52 @@
 package main
 
 import "fmt"
+import "math"
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-  size := (len(nums1) + len(nums2))
-  mid := size / 2
-  odd := size % 2 != 0
-  if odd { mid++ }
-  f := findNext(nums1, nums2)
-  var res float64
-  for {
-    pos, num := f()
-    if pos >= mid {
-      if odd {
-        res = float64(num)
-      } else {
-        _, num2 := f()
-        res = float64(num + num2) / 2.0
-      }
-      break
-    }
+  if len(nums1) > len(nums2) {
+    nums1, nums2 = nums2, nums1
   }
-  return res
-}
+  m, n := len(nums1), len(nums2)
+  imin, imax, halflen := 0, m, (m + n + 1) / 2
+  var i, j int
+  for imin <= imax {
+    i = (imin + imax) / 2
+    j = halflen - i
+    if i < imax && nums2[j - 1] > nums1[i] {
+      imin++
+    } else if i > imin && nums1[i - 1] > nums2[j] {
+      imax--
+    } else {
+      var maxLeft float64
+      if i == 0 {
+        maxLeft = float64(nums2[j - 1])
+      } else if j == 0 {
+        maxLeft = float64(nums1[i - 1])
+      } else {
+        maxLeft = math.Max(float64(nums1[i - 1]), float64(nums2[j - 1]))
+      }
+      if (m + n) % 2 != 0 {
+        return maxLeft
+      }
 
-func findNext(nums1 []int, nums2 []int) (func() (int, int)) {
-  var i, j, cur int
-  return func() (int, int) {
-    if i < len(nums1) && j < len(nums2) {
-      if nums1[i] < nums2[j] {
-        cur = nums1[i]
-        i++
+      var minRight float64
+      if i == m {
+        minRight = float64(nums2[j])
+      } else if j == n {
+        minRight = float64(nums1[i])
       } else {
-        cur = nums2[j]
-        j++
+        minRight = math.Min(float64(nums1[i]), float64(nums2[j]))
       }
-    } else if i < len(nums1) {
-      cur = nums1[i]
-      i++
-    } else if j < len(nums2){
-      cur = nums2[j]
-      j++
+
+      return (maxLeft + minRight) / 2
     }
-    return i + j, cur
   }
+  return 0.0
 }
 
 func main() {
   nums1 := []int{1,3}
-  nums2 := []int{2}
+  nums2 := []int{2, 5, 7}
   fmt.Println(findMedianSortedArrays(nums1, nums2))
 }
