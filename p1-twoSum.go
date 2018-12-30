798b33cf82f503b590d1b025439eac43c66079ec
cloudy9101 <cloudy9101@gmail.com>
2018-12-31 11:13:40 +1300

    P1:(2) Two Sum

--- p1-twoSum.go
+++ p1-twoSum.go
@@ -3,20 +3,16 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-  return twoSumIter(nums, target, 0)
-}
-
-func twoSumIter(nums []int, target int, i int) []int {
-  if len(nums) <= 1 {
-    return []int{}
-  }
-  a := nums[0]
-  for j, b := range(nums[1:]) {
-    if a + b == target {
-      return []int{i, i + j + 1}
+  m := make(map[int]int)
+  for i, num := range(nums) {
+    complement := target - num
+    j, ok := m[complement]
+    if ok {
+      return []int{j, i}
     }
+    m[num] = i
   }
-  return twoSumIter(nums[1:], target, i + 1)
+  return []int{}
 }
 
 func main() {
