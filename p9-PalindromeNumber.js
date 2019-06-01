6f8b1dc44b8a213354cbeb49611b4cf8588b26fd
cloudy9101 <cloudy9101@gmail.com>
2019-06-02 11:16:36 +1200

    ADD: p9-PalindromeNumber

---
+++ p9-PalindromeNumber.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if(x < 0) { return false; }
+  if(x === 0) { return true; }
+
+  var arr = [];
+  while(x > 0) {
+    arr.push(x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  for(var i = 0; i < arr.length / 2; i++) {
+    if(arr[i] != arr[arr.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
