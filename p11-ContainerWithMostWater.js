715ea0bd822ec2a28287bb3289e731243ea73314
cloudy9101 <cloudy9101@gmail.com>
2019-06-04 20:32:29 +1200

    ADD: p11-ContainerWithMostWater

---
+++ p11-ContainerWithMostWater.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+	var max = 0;
+	var i = 0;
+	var j = height.length - 1;
+	while(i < j) {
+		max = Math.max(Math.min(height[i], height[j]) * (j - i), max);
+		height[i] > height[j] ? j-- : i++;
+	}
+	return max;
+}
