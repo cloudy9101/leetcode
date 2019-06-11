1ba10b8593affaa8827de1ef223fdc143ecabe50
cloudy9101 <cloudy9101@gmail.com>
2019-06-11 15:16:01 +1200

    ADD: p23-MergeKSortedLists.v2

--- p23-MergeKSortedLists.js
+++ p23-MergeKSortedLists.js
@@ -9,50 +9,31 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
+
 var mergeKLists = function(lists) {
-  var head = new ListNode();
-  var cur = head;
-  var nullCount = 0;
-  var min = 0;
-  while(nullCount < lists.length) {
-    for(var i = 0; i < lists.length; i++) {
-      if(lists[min] === null || (lists[i] != null && lists[i].val < lists[min].val)) {
-        min = i;
-      }
-    }
-    if(lists[min] != null) {
-      cur.next = new ListNode(lists[min].val);
-      cur = cur.next;
-      lists[min] = lists[min].next;
-    }
-    if(lists[min] === null) {
-      nullCount++;
+  while(lists.length > 1) {
+    for(var i = 0; i < lists.length - 1; i += 2) {
+      lists.splice(i, 2, mergeTwoLists(lists[i], lists[i + 1]));
     }
   }
-  return head.next;
+  return lists.length > 0 ? lists[0] : null;
 };
 
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
-
-// var arr = [
-//   [1, 4, 5],
-//   [1, 3, 4],
-//   [2, 6]
-// ]
-var arr = [[]]
-
-var lists = [];
-
-for(var i = 0; i < arr.length; i++) {
-  var l = new ListNode(arr[i][0]);
-  lists.push(l);
-  for(var j = 0; j < arr[i].length - 1; j++) {
-    l.next = new ListNode(arr[i][j + 1]);
-    l = l.next;
+var mergeTwoLists = function(l1, l2) {
+  let res = new ListNode();
+  let cur = res;
+  while(l1 && l2){
+    if(l1.val <= l2.val){
+      cur.next = new ListNode(l1.val);
+      l1 = l1.next;
+      cur = cur.next;
+    }
+    else {
+      cur.next = new ListNode(l2.val);
+      l2 = l2.next;
+      cur = cur.next;
+    }
   }
-}
-console.log(lists);
-console.log(mergeKLists(lists))
+  cur.next = l1 || l2;
+  return res.next;
+};
